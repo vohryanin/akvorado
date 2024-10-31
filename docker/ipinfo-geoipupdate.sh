22f228c4c4ea9073e57d8ffc497017aa74cb8000
Vladimir Khryanin <v.xryanin@sigma-it.ru>
2024-10-31 10:56:13 +0000

    default conf

---
+++ docker/ipinfo-geoipupdate.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+trap exit TERM
+
+while true; do
+    for DATABASE in ${IPINFO_DATABASES}; do
+        if [ -f ${DATABASE}.mmdb ]; then
+            # Is it up-to-date?
+            LOCAL=$(sha256sum ${DATABASE}.mmdb | awk '{print $1}')
+            REMOTE=$(curl --silent https://ipinfo.io/data/free/${DATABASE}.mmdb/checksums?token=${IPINFO_TOKEN} \
+                | sed -n 's/.*"sha256": *"\([a-f0-9]*\)".*/\1/p')
+            if [ "$LOCAL" = "$REMOTE" ]; then
+                echo "${DATABASE}.mmdb is up-to-date"
+                continue
+            fi
+        fi
+        RESPONSE=$(curl \
+            --silent \
+            --write-out '%{http_code}' \
+            --remote-time \
+            --location \
+            --output "${DATABASE}.mmdb.new" \
+            "https://ipinfo.io/data/free/${DATABASE}.mmdb?token=${IPINFO_TOKEN}")
+        case "$RESPONSE" in
+            200)
+                echo "${DATABASE}.mmdb database downloaded in /data volume."
+                mv "${DATABASE}.mmdb.new" "${DATABASE}.mmdb"
+                ;;
+            *)
+                echo "Failed to download ${DATABASE}.mmdb database (HTTP error $RESPONSE)."
+                rm "${DATABASE}.mmdb.new" 2> /dev/null
+                ;;
+        esac
+    done
+
+    sleep "$UPDATE_FREQUENCY" &
+    wait $!
+done
